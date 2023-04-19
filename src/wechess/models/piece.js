ca377094cd2ad97eddffe677c611b0da22d0ae59
Ojerry <onotu128@gmail.com>
2023-04-19 15:59:00 +0100

    piece model

---
+++ src/wechess/models/piece.js
@@ -0,0 +1,37 @@
+class Piece{
+    constructor(name, isAttacked, color, id){
+        this.name = name
+        this.isAttacked = isAttacked // boolean
+        this.color = color
+        this.id = id
+    }
+
+    setSquare(newSquare) {
+        // set the square this piece is sitting top of. 
+        // on any given piece (on the board), there will always be a piece on top of it. 
+        // console.log(newSquare)
+        if (newSquare === undefined) {
+            this.squareThisPieceIsOn = newSquare
+            return 
+        }
+
+        if (this.squareThisPieceIsOn === undefined) {
+            this.squareThisPieceIsOn = newSquare
+            newSquare.setPiece(this)
+        }
+
+        const isNewSquareDifferent = this.squareThisPieceIsOn.x != newSquare.x || this.squareThisPieceIsOn.y != newSquare.y
+
+        if (isNewSquareDifferent) {
+            // console.log("set")
+            this.squareThisPieceIsOn = newSquare
+            newSquare.setPiece(this)
+        }
+    }
+
+    getSquare() {
+        return this.squareThisPieceIsOn
+    }
+}
+
+export default Piece
